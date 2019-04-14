0469f9fdeb91efba94fd7ed123ca8e0ea241e767
Sweta <swetap1012@gmail.com>
2019-04-14 14:40:27 -0400

    boilerplate -- create feature and core folder for components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Wizard from './core/components/Wizard/Wizard';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Wizard />
+      </div>
+    );
+  }
+}
+
+export default App;
