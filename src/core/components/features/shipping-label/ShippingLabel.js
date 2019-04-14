ed51baa9f98e647543185efe37444a2d90f52c1c
Sweta <swetap1012@gmail.com>
2019-04-14 14:50:39 -0400

    working on shipping-label component

---
+++ src/core/components/features/shipping-label/ShippingLabel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ShippingLabel extends Component {
+    render() {
+        return (
+            <div className="shipping-label">
+                ShippingLabel
+      </div>
+        );
+    }
+}
+
+export default ShippingLabel;
